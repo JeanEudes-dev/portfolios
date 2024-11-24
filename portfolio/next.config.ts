66218a833b65262632f3b7db1979320246f3e074
JeanEudes-dev <152700827+JeanEudes-dev@users.noreply.github.com>
2024-11-24 19:13:51 +0200

    update

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enable static export
-  basePath: "/<portfolios", // Replace <repository-name> with your repo name
+  basePath: "/portfolios", // Replace <repository-name> with your repo name
   images: {
     unoptimized: true, // Ensures images work in a static export
   },
