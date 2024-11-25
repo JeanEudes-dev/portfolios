979b04f9eec0e6837ebb62d3fcae0b3a7d897b8b
JeanEudes-dev <152700827+JeanEudes-dev@users.noreply.github.com>
2024-11-25 09:44:55 +0200

    ..

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // basePath: "/portfolios/portfolio",
-  // assetPrefix: "/portfolios/portfolio",
+  output: "export",
+  basePath: isProd ? "/out" : "",
+  assetPrefix: isProd ? "/out/" : "",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
