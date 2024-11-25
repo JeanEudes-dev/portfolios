0657d23352e6af7b8456a63f0922013f16f7b67f
JeanEudes-dev <152700827+JeanEudes-dev@users.noreply.github.com>
2024-11-25 21:25:19 +0200

    ...

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "" : "",
   assetPrefix: isProd ? "/" : "",
   reactStrictMode: true,
   images: {
